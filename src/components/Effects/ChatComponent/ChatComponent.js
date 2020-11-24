5071bd3d3f68fc8a79ff4edd2bf8564012674759
Kristijan <kristijan.pajtasev@hotmail.com>
2020-11-24 20:57:47 +0000

    using chat api

--- src/components/Effects/ChatComponent/ChatComponent.js
+++ src/components/Effects/ChatComponent/ChatComponent.js
@@ -2,6 +2,13 @@ import {useEffect} from 'react';
 import ChatAPI from './ChatAPI';
 
 function ChatComponent() {
+    useEffect(() => {
+     ChatAPI.subscribe();
+
+     return () => {
+         ChatAPI.unsubscribe();
+     }
+    })
     return <div>Chat Component</div>
 }
 
